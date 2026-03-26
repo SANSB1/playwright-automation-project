c3d706e7a74fb5c29ccec6cc63ff030df5f8ae5b
Sanatan07 <bsanatan51@gmail.com>
2026-03-26 16:24:26 +0530

    Initial Playwright automation project

---
+++ tests/checkout.spec.js
@@ -0,0 +1,26 @@
+const { test, expect } = require('@playwright/test');
+
+test('Checkout Flow', async ({ page }) => {
+
+  await page.goto('https://www.saucedemo.com/');
+
+  await page.fill('#user-name', 'standard_user');
+  await page.fill('#password', 'secret_sauce');
+  await page.click('#login-button');
+
+  await page.click('#add-to-cart-sauce-labs-backpack');
+  await page.click('.shopping_cart_link');
+
+  await page.click('#checkout');
+
+  await page.fill('#first-name', 'Sans');
+  await page.fill('#last-name', 'User');
+  await page.fill('#postal-code', '411001');
+
+  await page.click('#continue');
+
+  await page.click('#finish');
+
+  await expect(page.locator('.complete-header')).toHaveText('Thank you for your order!');
+
+});
